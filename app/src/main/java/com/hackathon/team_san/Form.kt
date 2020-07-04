0ece1d2da4128cd73f23c7c424f83c506d384f1f
AKSHAY <akshayroy.ar@gmail.com>
2020-07-05 02:35:01 +0530

    form

--- app/src/main/java/com/hackathon/team_san/Form.kt
+++ app/src/main/java/com/hackathon/team_san/Form.kt
@@ -1,11 +1,22 @@
 package com.hackathon.team_san
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Form:AppCompatActivity() {
+    lateinit var Next:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.form)
+            supportActionBar?.title = "Register Yourself"
+            supportActionBar?.setHomeButtonEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+Next=findViewById(R.id.btnNext)
+        Next.setOnClickListener {
+            var i= Intent(this@Form,Form_next::class.java)
+            startActivity(i)
+        }
     }
 }
