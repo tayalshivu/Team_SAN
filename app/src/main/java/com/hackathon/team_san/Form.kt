ceddf282f5b75380b1fa73a84a0d4d56ede52b8a
AKSHAY <akshayroy.ar@gmail.com>
2020-07-04 14:45:50 +0530

    form

---
+++ app/src/main/java/com/hackathon/team_san/Form.kt
@@ -0,0 +1,11 @@
+package com.hackathon.team_san
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Form:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
